bd032b1728400e9dc23b42633a5269a77e9b7169
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-06 23:04:02 +0100

    buttons object

---
+++ src/components/Calculator/buttons.js
@@ -0,0 +1,78 @@
+export const buttons = [
+	{
+		label: "c",
+		type: "symbol",
+	},
+	{
+		label: "+/-",
+		type: "symbol",
+	},
+	{
+		label: "%",
+		type: "symbol",
+	},
+	{
+		label: "/",
+		type: "symbol",
+	},
+	{
+		label: 7,
+		type: "number",
+	},
+	{
+		label: 8,
+		type: "number",
+	},
+	{
+		label: 9,
+		type: "number",
+	},
+	{
+		label: "*",
+		type: "symbol",
+	},
+	{
+		label: 4,
+		type: "number",
+	},
+	{
+		label: 5,
+		type: "number",
+	},
+	{
+		label: 6,
+		type: "number",
+	},
+	{
+		label: "-",
+		type: "symbol",
+	},
+	{
+		label: 1,
+		type: "number",
+	},
+	{
+		label: 2,
+		type: "number",
+	},
+	{
+		label: 3,
+		type: "number",
+	},
+	{
+		label: "+",
+		type: "symbol",
+	},
+	{
+		label: 0,
+		type: "number",
+	},
+	{
+		label: ".",
+		type: "symbol",
+	},
+	{
+		label: "=",
+		type: "symbol",
+	},
+];
